b3b4c27e32185fbfd1ef324834be4aa25284e1ed
dennis <info.dennis.durmus@gmail.com>
2024-08-27 14:31:56 +0200

    Registrieren mit Mobilnummer und Verificationcode

--- main.dart
+++ main.dart
@@ -1,1 +1,16 @@
-void main() {}
+import 'dart:io';
+
+void main() {
+  int mobileNumber = 0;
+  int code = 0;
+  print("Eingabe Handy Rufnummer:");
+  mobileNumber = int.parse(stdin.readLineSync()!);
+  print("Eingabe Pin Code:");
+  code = int.parse(stdin.readLineSync()!);
+  if (mobileNumber == 015734818527 && code == 1708) {
+    print("Verification Erfolgreich");
+  } else {
+    print(
+        "Falsche Rufnummer oder Pin, Bitte Rufnummer und Pin erneut Eingeben");
+  }
+}
